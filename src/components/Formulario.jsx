34114437f5cb04f752bf2c243c4997401cab5dc8
Nicolas GaliaM <nicolasgaliam@gmail.com>
2022-10-16 19:03:16 -0300

    terminamos punto 8

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,15 +1,16 @@
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
+import ListGroup from "react-bootstrap/ListGroup";
 import Form from "react-bootstrap/Form";
 import Swal from "sweetalert2";
-import { validacionNombre } from "../validaciones";
+import { validacionApellido, validacionNombre, validarEmail, validarDni } from "../validaciones";
 
 const Formulario = () => {
   const [validar, setValidar] = useState(false);
 
   const [nombre, setNombre] = useState("");
   const [apellido, setApellido] = useState("");
-  const [DNI, setDNI] = useState();
+  const [dni, setDni] = useState();
   const [email, setEmail] = useState("");
 
   const [cliente, setCliente] = useState({});
@@ -17,13 +18,18 @@ const Formulario = () => {
   const validarDatos = (e) => {
     e.preventDefault();
     e.stopPropagation();
-    if (validacionNombre(nombre)) {
+    if (!validacionNombre(nombre) || 
+    !validacionApellido(apellido) || 
+    !validarDni(dni) || 
+   !validarEmail(email)){
+      return;
+    } else {
       setValidar(true);
       setCliente({
         nombre,
         apellido,
         email,
-        DNI,
+        dni,
       });
       Swal.fire({
         icon: "success",
@@ -33,6 +39,9 @@ const Formulario = () => {
       });
     }
     setNombre("");
+    setApellido("");
+    setDni("");
+    setEmail("");
   };
 
   return (
@@ -45,6 +54,7 @@ const Formulario = () => {
           <Form.Label>Nombre</Form.Label>
           <Form.Control
             type="text"
+            value={nombre}
             placeholder="por ej: Nicolas"
             onChange={(e) => setNombre(e.target.value)}
           />
@@ -54,6 +64,7 @@ const Formulario = () => {
           <Form.Label>Apellido</Form.Label>
           <Form.Control
             type="text"
+            value={apellido}
             placeholder="por ej: Gonzalez"
             required
             onChange={(e) => setApellido(e.target.value)}
@@ -64,9 +75,10 @@ const Formulario = () => {
           <Form.Label>DNI</Form.Label>
           <Form.Control
             type="number"
+            value={dni}
             placeholder="40300200"
             required
-            onChange={(e) => setDNI(e.target.value)}
+            onChange={(e) => setDni(e.target.value)}
           />
         </Form.Group>
 
@@ -74,6 +86,7 @@ const Formulario = () => {
           <Form.Label>Email</Form.Label>
           <Form.Control
             type="email"
+            value={email}
             placeholder="nicolas@gmail.com"
             required
             onChange={(e) => setEmail(e.target.value)}
@@ -84,6 +97,15 @@ const Formulario = () => {
           Submit
         </Button>
       </Form>
+      {
+        validar ?   
+        <ListGroup>
+        <ListGroup.Item>{cliente.nombre}</ListGroup.Item>
+        <ListGroup.Item>{cliente.apellido}</ListGroup.Item>
+        <ListGroup.Item>{cliente.dni}</ListGroup.Item>
+        <ListGroup.Item>{cliente.email}</ListGroup.Item>     
+        </ListGroup> : null
+      }
     </div>
   );
 };
--- src/validaciones.js
+++ src/validaciones.js
@@ -2,7 +2,7 @@ import Swal from "sweetalert2";
 
 
 export const validacionNombre = (nombre)=>{
-if(nombre !== '' ){
+if(nombre !== '' && isNaN(nombre)){
     return true;
 } else {
     Swal.fire({
@@ -14,7 +14,42 @@ return false;
 }
 };
 
-export const validacionApellido = ()=> {
-    if (apellido !== '')
+export const validacionApellido = (apellido)=> {
+    if (apellido !== '' && isNaN(apellido))
+  {  
     return true;
+}else {
+    Swal.fire({
+        icon: "error",
+        title: "Apellido Invalido",
+        text: "escribe un apellido correcto"
+        })
+        return false;
+    }}
+export const validarEmail = (email)=> {
+    const regEx =
+      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+    if (email !== '' &&  regEx.test(email)){
+        return true;
+    } else {
+        Swal.fire({
+            icon: "error",
+            title: "Email Invalido",
+            text: "escribe un Email correcto"
+    }) 
+    return false;
+} }   
+
+export const validarDni = (dni)=> {
+    const regEx = /^[\d]{1,3}\.?[\d]{3,3}\.?[\d]{3,3}$/;
+    if (dni !== '' && regEx.test(dni) ){
+        return true;
+    } else {
+        Swal.fire({
+            icon: "error",
+            title: "Dni Invalido",
+            text: "escribe un Dni correcto"
+    }) 
+    return false;
+    }
 }
